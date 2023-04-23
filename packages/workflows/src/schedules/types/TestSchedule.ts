c2b83f874168effc572f44dca21f81a971cd2197
julipup <dingolediego@gmail.com>
2023-04-23 13:23:19 +0300

    Change TestSchedule's interval to 1 day

--- packages/workflows/src/schedules/types/TestSchedule.ts
+++ packages/workflows/src/schedules/types/TestSchedule.ts
@@ -3,7 +3,7 @@ import { AbstractSchedule } from "../AbstractSchedule";
 
 export const TestSchedule: AbstractSchedule = {
     id: "test-schedule2",
-    intervals: [{ every: '1s' }],
+    intervals: [{ every: '1d' }],
     handler: searchByTags,
     args: [{ query: "test" }]
 };
